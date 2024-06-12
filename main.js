d2b0b5fa0f72002ded342d502dfaaa150e1c47c1
zakiroc <164227048+zakiroc@users.noreply.github.com>
2024-06-12 22:18:11 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+let pr = prompt('birinchi sonni kiriting')
+let pr_2 = prompt('ikkinchi sonni kiriting')
+
+if(pr>pr_2){
+    alert(`eng kotta song ${pr}`)
+}if(pr_2>pr){
+    alert(`eng kotta song ${pr_2}`)
+}if(pr==pr_2){
+    alert(`ikkalasi bir biriga teng`)
+}
